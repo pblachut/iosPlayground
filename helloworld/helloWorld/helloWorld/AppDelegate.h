f08fcff75a715f8b025d36155ac89d7790880e65
Konto Piotrek <blachut.piotr@gmail.com>
2016-01-12 20:39:03 +0100

    fix gitignore

---
+++ helloworld/helloWorld/helloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  helloWorld
+//
+//  Created by Konto Piotrek on 12/01/16.
+//  Copyright © 2016 Konto Piotrek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
