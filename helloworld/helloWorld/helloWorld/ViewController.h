a1d07bf1c702faa3a7424065e3dde668c2dcf143
Konto Piotrek <piotrek@Mac-mini-Konto.local>
2016-01-12 20:24:54 +0100

    initial commit

---
+++ helloworld/helloWorld/helloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  helloWorld
+//
+//  Created by Konto Piotrek on 12/01/16.
+//  Copyright © 2016 Konto Piotrek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
